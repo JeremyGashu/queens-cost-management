4ba52e765530b7843c39161affa8f8b35cb92726
JeremyGashu <gearmias@gmail.com>
2020-09-23 11:23:35 +0300

    h

--- api/routes/entry.js
+++ api/routes/entry.js
@@ -4,19 +4,19 @@ const AuthChecker = require('../middlewares/auth_user')
 
 const router = express.Router()
 
-router.get('/',AuthChecker.authNeeded, EntriesController.get_all_entries)
+router.get('/', EntriesController.get_all_entries)
 
 router.get('/:entry_id' ,EntriesController.entries_by_id)
 
-router.post('/',AuthChecker.userAuthChecker, EntriesController.create_entry)
+router.post('/', EntriesController.create_entry)
 
-router.delete('/:entry_id', AuthChecker.authNeeded, EntriesController.delete_entry)
+router.delete('/:entry_id',  EntriesController.delete_entry)
 
-router.patch('/user/:entry_id', AuthChecker.userAuthChecker, EntriesController.entry_checked_by_user)
+router.patch('/user/:entry_id',  EntriesController.entry_checked_by_user)
 
-router.patch('/marketing/:entry_id',AuthChecker.marketingAuthChecker, EntriesController.entry_checked_by_marketing_manager)
+router.patch('/marketing/:entry_id', EntriesController.entry_checked_by_marketing_manager)
 
-router.patch('/manager/:entry_id', AuthChecker.managerAuthChecker, EntriesController.entry_checked_by_general_manager)
+router.patch('/manager/:entry_id',EntriesController.entry_checked_by_general_manager)
 
 router.use(EntriesController.error_handler)
 
