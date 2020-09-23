8b857420dd2d29c9fba640854afc491b78abc26e
JeremyGashu <gearmias@gmail.com>
2020-09-23 09:18:10 +0300

    h

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
@@ -27,7 +26,6 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 
-app.use(morgan('dev'))
 
 app.use('/suppliers', suppliersRoute)
 app.use('/entries', entriesRoute)
